932e378bd4415c8696e8f9a3f1e4e36e7d91a8da
Javier Pons <jpescandell@hotmail.com>
2021-02-17 12:08:46 +0200

    added router post

---
+++ api.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const FarmDb = require('./models/model');
+
+
+
+    router.post("/api/farm/", function (req, res) {
+            // if(req.body !== null && req.body !== undefined){
+                const name = req.body.name;
+                const tel = req.body.tel;
+                const gender = req.body.gender
+                    console.log(name);
+                    console.log(tel);
+                    console.log(gender);
+                FarmDb.create({name: name, tel: tel, gender: gender}).then((data)=> {
+                    res.send(data)
+                })
+        
+            // }
+        })
+
+
+        module.exports = router;
