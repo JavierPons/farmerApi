6d080296fbc5b867ec92713ef93d7d9601529e87
Javier Pons <jpescandell@hotmail.com>
2021-02-17 12:11:06 +0200

    cors issue fixed and added endpoint

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require("express");
-
+const fileupload = require("express-fileupload");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const route = require('./api');
 
 const app = express();
 require('./models/index')
+
+app.use(fileupload())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
-// var corsOptions = {
-//     origin: "http://localhost:8081"
-//   };
 
-// app.use(cors(corsOptions));
+// Resolving CORS problems
+app.use(cors())
+
+
+
+
+app.use("/api", route);
 
 // DB
+
 const db = require('./models/index')
 async ()=> {
     try{
@@ -24,11 +31,26 @@ async ()=> {
 };
 
 
+app.post("/farm", (req, res) => {
+    // if(req.body !== null && req.body !== undefined){
+        const name = req.body.name;
+        const tel = req.body.tel;
+        const gender = req.body.gender
+
+        FarmDb.create({name: name, tel: tel, gender: gender}).then((data)=> {
+            res.send(data)
+        })
+
+    // }
+})
+
 app.get("/", (req,res) => {
     res.json({message: "This is the api for farmer search"})
 })
 
+
+
 const PORT = process.env.PORT || 3004;
 app.listen(PORT, ()=> {
-    console.log("Running...");
+    console.log(`Running... ${PORT}`);
 })
